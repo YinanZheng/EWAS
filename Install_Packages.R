ffff72240fd6f6b0e7be0f2f5db14f05c454d1bb
Yinan Zheng <y-zheng@northwestern.edu>
2017-07-06 12:30:28 -0500

    updates

---
+++ Install_Packages.R
@@ -0,0 +1,24 @@
+
+installPackages <- function(packages, update = FALSE)
+{
+  installedPackages <- installed.packages()[,"Package"]
+  source("http://bioconductor.org/biocLite.R")
+  
+  if(update)
+  {
+    biocLite(packages, suppressUpdates = TRUE)
+  } else {
+    new.packages <- packages[!(packages %in% installedPackages)]
+    if(length(new.packages)) 
+    {
+      biocLite(new.packages, suppressUpdates = TRUE)
+    } else {
+      message("All required packages have been installed.")
+    }
+  }
+}
+
+list.of.packages <- c("ggplot2", "gridBase", "qualityTools", "XLConnectJars", "XLConnect", "Hmisc", "MASS", "sandwich", "lmtest",
+                      "qvalue", "IlluminaHumanMethylationEPICanno.ilm10b2.hg19")
+
+installPackages(list.of.packages)
