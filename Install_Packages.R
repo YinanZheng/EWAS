c38bd9e361d48b258cc7f64c344ba1cd192efac3
YinanZheng <y-zheng@northwestern.edu>
2018-08-31 01:18:53 -0500

    BiocManager

--- Install_Packages.R
+++ Install_Packages.R
@@ -4,18 +4,17 @@ suppressWarnings(rm(installPackages))
 installPackages <- function(packages, update = FALSE)
 {
   installedPackages <- installed.packages()[,"Package"]
-  source("http://bioconductor.org/biocLite.R")
-  
+
   if(update)
   {
     message("Reinstalling all required packages.")
-    biocLite(packages, suppressUpdates = TRUE)
+    BiocManager::install(packages, ask = FALSE)
   } else {
     new.packages <- packages[!(packages %in% installedPackages)]
     if(length(new.packages)) 
     {
       message(paste0(new.packages, collapse = ", "), " are missing...")
-      biocLite(new.packages, suppressUpdates = TRUE)
+      BiocManager::install(new.packages, ask = FALSE)
     } else {
       message("All required packages have been installed.")
     }
